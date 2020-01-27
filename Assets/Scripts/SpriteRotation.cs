d7dcb06c12e151f07d7261273856749756527abf
BenjaminMalloneeC <ben.mallonee.c@gmail>
2020-01-27 12:12:45 -0700

    Add files via upload

---
+++ Assets/Scripts/SpriteRotation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteRotation : MonoBehaviour
+{
+    //Create a variable for our transform compenent
+    private Transform tf;
+
+    //Create a variabe for the degree we rotate in one frame draw
+    public float turnSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Load our transform component into our variable
+        tf = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            tf.Rotate(0, 0, +turnSpeed);
+        }
+    }
+}
